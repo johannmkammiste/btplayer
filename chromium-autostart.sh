6ddd58d9cac4d2cd4f50b2f1c69ca12234e8970c
Johann Mattias Kammiste <81474277+johannmkammiste@users.noreply.github.com>
2025-05-16 20:44:23 +0300

    Update chromium-autostart.sh

--- chromium-autostart.sh
+++ chromium-autostart.sh
@@ -2,7 +2,7 @@
 # Autostart script for kiosk mode, based on @AYapejian: https://github.com/MichaIng/DietPi/issues/1737#issue-318697621
 # 1. Define your project directory
 #    IMPORTANT: Change this to the absolute path of your project directory
-PROJECT_DIR="/root/btplayer"
+PROJECT_DIR="/root/btplayer-main"
 
 # 2. Navigate to your project directory
 cd "$PROJECT_DIR" || echo "Failed to cd to $PROJECT_DIR"
